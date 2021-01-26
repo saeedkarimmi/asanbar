1f3dfecab6d79350cbe3c4a434df076646c5e150
Saeed Karimi <saeedkarimi454@gmail.com>
2021-01-26 17:38:42 +0330

    make a method for getting post`s categories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,14 @@ class Post extends Model
     {
         return $this->belongsToMany(Tag::class, 'post_tags');
     }
+
+    public function categories()
+    {
+        return Post::query()->join('post_tags', 'post_tags.post_id', 'posts.id')
+            ->leftjoin('tags', 'tags.id', 'post_tags.post_id')
+            ->leftjoin('category_tags', 'category_tags.tag_id', 'tags.id')
+            ->leftjoin('categories', 'categories.id', 'category_tags.category_id')
+            ->where('posts.id' , $this->id)
+            ->distinct();
+    }
 }
