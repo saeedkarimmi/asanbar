44395d35c203a531aa6bb203d2282a7355bf16d9
Saeed Karimi <saeedkarimi454@gmail.com>
2021-01-26 17:44:35 +0330

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\API\V1\PostController as V1PostController;
+use App\Http\Controllers\API\V2\PostController as V2PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1'/*, 'namespace' => 'Api\V1'*/], function () {
+    Route::resource('posts', V1PostController::class)->only(['index', 'show']);
+});
+
+Route::group(['prefix' => 'v2'/*, 'namespace' => 'Api\V2'*/], function () {
+    Route::resource('posts', V2PostController::class)->only(['index', 'show']);
 });
