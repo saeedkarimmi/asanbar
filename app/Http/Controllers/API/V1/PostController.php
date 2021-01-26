d75c25f496c902a76bce80a56a90d6a3d16794a2
Saeed Karimi <saeedkarimi454@gmail.com>
2021-01-26 17:39:11 +0330

    add index adn show methods

--- app/Http/Controllers/API/V1/PostController.php
+++ app/Http/Controllers/API/V1/PostController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::query()->limit(30)->paginate();
+        return PostResource::collection($posts);
+    }
+
+    public function show(Post $post)
+    {
+        return new PostResource($post);
+    }
 }
--- app/Http/Controllers/API/V2/PostController.php
+++ app/Http/Controllers/API/V2/PostController.php
@@ -2,10 +2,23 @@
 
 namespace App\Http\Controllers\API\V2;
 
+use App\Events\EndOfIndexing;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\PostResource;
+use App\Models\Post;
+use \App\Http\Controllers\API\V1\PostController as V1PostController;
 use Illuminate\Http\Request;
 
-class PostController extends Controller
+class PostController extends V1PostController
 {
-    //
+    public function index()
+    {
+        $posts = Post::/*on('mysql2')->*/limit(1)->get()/*->paginate()*/;
+        return PostResource::collection($posts);
+    }
+
+//    public function show(Post $post)
+//    {
+//        return new PostResource($post);
+//    }
 }
